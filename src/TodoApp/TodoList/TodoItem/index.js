af3348ff33d0c480b964b23bbdae9d7f3190f790
Evgenii Kinchin <evgen4in@gmail.com>
2018-11-20 23:45:05 +0500

    todoitem sizing

--- src/TodoApp/TodoList/TodoItem/index.js
+++ src/TodoApp/TodoList/TodoItem/index.js
@@ -3,14 +3,14 @@ import './style.css';
 
 const TodoItem = ({todo, onDone, onRemove}) => {
   return (
-      <div className="row justify-content-md-center align-items-center m-3"> 
+      <div className="row justify-content-md-center align-items-center m-3 text-left"> 
         <div className={todo.isDone?"card col bg-light":"card col bg-light"}>    
-          <div className={todo.isDone?"card-body font-weight-light bg-light h5 text-muted":"card-body font-weight-normal bg-light h5 text-body"}>{todo.text}</div>
+          <div className={todo.isDone?"card-text font-weight-light bg-light text-muted":"card-text font-weight-normal bg-light text-body"}>{todo.text}</div>
         </div>
-        <div className="col">
+        <div className="col-1">
           <div className="btn-group">
-            <button type="button" className={todo.isDone?"btn btn-secondary":"btn btn-success"} onClick={onDone(todo.id)}>{todo.isDone?"Undo":"Done"}</button>
-            <button type="button" className="btn btn-warning" onClick={onRemove(todo.id)}>Del</button>
+            <button type="button" className={todo.isDone?"btn btn-sm btn-secondary":"btn btn-sm btn-success"} onClick={onDone(todo.id)}>{todo.isDone?"Undo":"Done"}</button>
+            <button type="button" className="btn btn-sm btn-warning" onClick={onRemove(todo.id)}>Del</button>
           </div>
         </div>
       </div>
