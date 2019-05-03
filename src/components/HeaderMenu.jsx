f28d619a8266b79fba808388fbf141cff0c7ac02
Evgenii Kinchin <evgen4in@gmail.com>
2019-05-03 11:52:41 +0500

    FileUploader добавлен в header

--- src/components/HeaderMenu.jsx
+++ src/components/HeaderMenu.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-no-bind */
 import React from 'react';
 import propTypes from 'prop-types';
-
 import {
   Row, Button, Container, ButtonGroup,
 } from 'react-bootstrap';
+import FileUploader from './FileUploader';
+
 
 const HeaderMenu = ({
   onDownload, onUpload, onReverse, isReverse,
@@ -12,6 +13,7 @@ const HeaderMenu = ({
   <Container fluid="true">
     <Row className="justify-content-between align-items-center" noGutters="true">
       <ButtonGroup>
+        <FileUploader />
         <Button size="sm" type="file" as="input" onChange={onUpload} />
         <Button size="sm" variant="success" onClick={onDownload}>скачать</Button>
         <Button size="sm" variant="primary" onClick={onReverse}>
