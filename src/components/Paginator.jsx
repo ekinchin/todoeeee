66624cf2882bb529440e2bcae723a8e10047bf89
ekinchin <evgen4in@gmail.com>
2020-01-03 18:20:56 +0500

    Пагинатор улучшен

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -9,33 +9,51 @@ import {
 const Paginator = ({
   currentPage, pageCount, onChangePage, todosOnPage, onChangeTodosOnPage,
 }) => {
-  const items = [];
-  for (let i = 1; i <= pageCount; i += 1) {
-    items.push(i);
+  let items = [];
+  if (pageCount <= 5) {
+    for (let i = 1; i <= pageCount; i += 1) {
+      items.push(i);
+    }
+  } else {
+    for (let i = currentPage - 2; i <= currentPage + 2; i += 1) {
+      items.push(i);
+    }
+    if (items[0] < 1) {
+      items = items.map(item => item - items[0] + 1);
+    }
+    if (items[4] > pageCount) {
+      items = items.map(item => item - items[4] + pageCount);
+    }
   }
   return (
     <Container>
-      <Row className="justify-content-md-center align-items-top text-left my-3" noGutters="true">
-        <Col className="col-auto m-3">
+      <Row className="justify-content-md-center align-items-top text-left" noGutters="true">
+        <Col className="col-auto">
           {
             pageCount !== 1
               ? (
                 <Pagination size="sm">
+                  <Pagination.Item className={currentPage === 1 ? 'disabled' : null} key={1} onClick={onChangePage(1)}>
+                      Первая
+                  </Pagination.Item>
                   {
-                    items.map(
-                      item => (
-                        <Pagination.Item className={item === currentPage ? 'active' : null} key={item} onClick={onChangePage(item)}>
-                          {item}
-                        </Pagination.Item>
-                      ),
-                    )
+                    items.map(item => (
+                      <Pagination.Item className={currentPage === item ? 'active' : null} key={item} onClick={onChangePage(item)}>
+                        { currentPage === item ? `${item} из ${pageCount}` : item }
+                      </Pagination.Item>
+                    ))
                   }
+                  <Pagination.Item className={currentPage === pageCount ? 'disabled' : null} key={pageCount} onClick={onChangePage(pageCount)}>
+                      Последняя
+                  </Pagination.Item>
                 </Pagination>
               )
               : null
           }
         </Col>
-        <Col className="col-auto m-3">
+      </Row>
+      <Row className="justify-content-md-center align-items-top text-left" noGutters="true">
+        <Col className="col-auto">
           <span className="mr-1">Записей на странице:</span>
           <ButtonGroup size="sm" vertical>
             <DropdownButton size="sm" as={ButtonGroup} title={todosOnPage}>
