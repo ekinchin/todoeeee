63d19898cb5583eac1b0d4364c16b9c58c5d3a3a
ekinchin <evgen4in@gmail.com>
2020-03-31 19:46:17 +0500

    переделал TodoEdit в функциональный компонент

--- src/components/TodoEdit.jsx
+++ src/components/TodoEdit.jsx
@@ -1,189 +1,245 @@
-import React, { Component } from 'react';
-import fileDownload from 'js-file-download';
+import React from 'react';
+import propTypes from 'prop-types';
 import { Container } from 'react-bootstrap';
 import TodoList from './TodoList';
 import TodoInput from './TodoInput';
 import HeaderMenu from './HeaderMenu';
 import Paginator from './Paginator';
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
+const TodoEdit = ({
+  todos, isReverse, pageNumber, todosOnPage, handlerAppendTodo, handlerDoneTodo, handlerRemoveTodo,
+  onChangePage, onChangeTodosOnPage, handlerReverseSelect, handlerFileDownload, handleFileUpload,
+}) => {
+  const input = React.createRef();
+  const todosDir = isReverse ? todos.map(todo => todo).reverse() : todos.map(todo => todo);
+  const todosView = todosDir.slice((pageNumber - 1) * todosOnPage, pageNumber * todosOnPage);
+  const pageCount = todos.length === 0 ? 1 : Math.ceil(todos.length / todosOnPage);
 
-class TodoEdit extends Component {
-  constructor(props) {
-    super(props);
-    this.input = React.createRef();
-  }
+  return (
+    <Container fluid="falses">
+      <TodoInput
+        input={input}
+        onAppend={handlerAppendTodo}
+      />
+      <TodoList
+        todos={todosView.filter(todo => todo.isDone === false)}
+        onDone={handlerDoneTodo}
+        onRemove={handlerRemoveTodo}
+      />
+      <TodoList
+        todos={todosView.filter(todo => todo.isDone === true)}
+        onDone={handlerDoneTodo}
+        onRemove={handlerRemoveTodo}
+      />
+      <Paginator
+        currentPage={pageNumber}
+        pageCount={pageCount}
+        onChangePage={onChangePage}
+        todosOnPage={todosOnPage}
+        onChangeTodosOnPage={onChangeTodosOnPage}
+        isReverse={isReverse}
+        onReverse={handlerReverseSelect}
+      />
+      <HeaderMenu
+        onDownload={handlerFileDownload}
+        onUpload={handleFileUpload}
+      />
+    </Container>
+  );
+};
 
-  state = {
-    todos: [],
-    isReverse: false,
-    pageNumber: 1,
-    todosOnPage: 10,
-  }
+// class TodoEdit extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.input = React.createRef();
+//   }
 
-  componentDidMount() {
-    let saved = [];
-    let isReverse = false;
-    if (typeof localStorage.todos === 'undefined') {
-      saved = '[]';
-    } else {
-      saved = localStorage.todos;
-    }
-    if (typeof localStorage.isReverse === 'undefined') {
-      isReverse = false;
-    } else {
-      // eslint-disable-next-line prefer-destructuring
-      isReverse = localStorage.isReverse;
-    }
+//   state = {
+//     todos: [],
+//     isReverse: false,
+//     pageNumber: 1,
+//     todosOnPage: 10,
+//   }
 
-    this.setState(
-      {
-        todos: JSON.parse(saved),
-        isReverse: JSON.parse(isReverse),
-      },
-    );
-    this.input.current.focus();
-  }
+//   componentDidMount() {
+//     let saved = [];
+//     let isReverse = false;
+//     if (typeof localStorage.todos === 'undefined') {
+//       saved = '[]';
+//     } else {
+//       saved = localStorage.todos;
+//     }
+//     if (typeof localStorage.isReverse === 'undefined') {
+//       isReverse = false;
+//     } else {
+//       // eslint-disable-next-line prefer-destructuring
+//       isReverse = localStorage.isReverse;
+//     }
 
-  componentDidUpdate() {
-    const { todos, isReverse } = this.state;
-    localStorage.setItem('todos', JSON.stringify(todos));
-    localStorage.setItem('isReverse', JSON.stringify(isReverse));
-  }
+//     this.setState(
+//       {
+//         todos: JSON.parse(saved),
+//         isReverse: JSON.parse(isReverse),
+//       },
+//     );
+//     this.input.current.focus();
+//   }
 
-  handlerAppendTodo = () => {
-    const {
-      todos, todosOnPage, isReverse,
-    } = this.state;
-    const input = this.input.current.value;
-    if (input === '') return;
-    this.input.current.value = '';
-    this.input.current.focus();
-    const pages = Math.ceil((todos.length + 1) / todosOnPage);
+//   componentDidUpdate() {
+//     const { todos, isReverse } = this.state;
+//     localStorage.setItem('todos', JSON.stringify(todos));
+//     localStorage.setItem('isReverse', JSON.stringify(isReverse));
+//   }
 
-    this.setState(prevState => ({
-      todos: [
-        ...prevState.todos,
-        {
-          id: new Date().getTime(),
-          date: new Date().toLocaleString(),
-          text: input,
-          isDone: false,
-        },
-      ],
-      pageNumber: isReverse ? 1 : pages,
-    }));
-  };
+//   handlerAppendTodo = () => {
+//     const {
+//       todos, todosOnPage, isReverse,
+//     } = this.state;
+//     const input = this.input.current.value;
+//     if (input === '') return;
+//     this.input.current.value = '';
+//     this.input.current.focus();
+//     const pages = Math.ceil((todos.length + 1) / todosOnPage);
 
-  handlerDoneTodo = id => () => {
-    this.setState(prevState => ({
-      todos: prevState.todos.map(
-        todo => (todo.id === id ? {
-          ...todo,
-          isDone: !todo.isDone,
-        } : todo),
-      ),
-    }));
-  };
+//     this.setState(prevState => ({
+//       todos: [
+//         ...prevState.todos,
+//         {
+//           id: new Date().getTime(),
+//           date: new Date().toLocaleString(),
+//           text: input,
+//           isDone: false,
+//         },
+//       ],
+//       pageNumber: isReverse ? 1 : pages,
+//     }));
+//   };
 
-  handlerRemoveTodo = id => () => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(
-        todo => todo.id !== id,
-      ),
-      pageNumber: 1,
-    }));
-  };
+//   handlerDoneTodo = id => () => {
+//     this.setState(prevState => ({
+//       todos: prevState.todos.map(
+//         todo => (todo.id === id ? {
+//           ...todo,
+//           isDone: !todo.isDone,
+//         } : todo),
+//       ),
+//     }));
+//   };
 
-  handlerFileDownload = () => {
-    const { todos } = this.state;
-    fileDownload(JSON.stringify(todos), 'history.json');
-  }
+//   handlerRemoveTodo = id => () => {
+//     this.setState(prevState => ({
+//       todos: prevState.todos.filter(
+//         todo => todo.id !== id,
+//       ),
+//       pageNumber: 1,
+//     }));
+//   };
 
-  handlerReverseSelect = () => {
-    this.setState(prevState => ({
-      isReverse: !prevState.isReverse,
-      todos: prevState.todos,
-    }));
-  }
+//   handlerFileDownload = () => {
+//     const { todos } = this.state;
+//     fileDownload(JSON.stringify(todos), 'history.json');
+//   }
 
-  handleFileUpload = (e) => {
-    const fileType = 'application/json';
-    const blob = e.target.files[0];
-    if (blob !== undefined && e.target.files[0].type === fileType) {
-      const reader = new FileReader();
-      reader.readAsText(blob, 'UTF-8');
-      reader.onload = (evt) => {
-        try {
-          const result = JSON.parse(evt.target.result);
-          this.setState({
-            todos: result,
-          });
-          // eslint-disable-next-line no-alert
-          alert('Загрузка выполнена.');
-        // eslint-disable-next-line no-alert
-        } catch { alert('Ошибка парсинга'); }
-      };
-    }
-  }
+//   handlerReverseSelect = () => {
+//     this.setState(prevState => ({
+//       isReverse: !prevState.isReverse,
+//       todos: prevState.todos,
+//     }));
+//   }
 
-  onChangePage = page => () => {
-    this.setState({
-      pageNumber: page,
-    });
-  }
+//   handleFileUpload = (e) => {
+//     const fileType = 'application/json';
+//     const blob = e.target.files[0];
+//     if (blob !== undefined && e.target.files[0].type === fileType) {
+//       const reader = new FileReader();
+//       reader.readAsText(blob, 'UTF-8');
+//       reader.onload = (evt) => {
+//         try {
+//           const result = JSON.parse(evt.target.result);
+//           this.setState({
+//             todos: result,
+//           });
+//           // eslint-disable-next-line no-alert
+//           alert('Загрузка выполнена.');
+//         // eslint-disable-next-line no-alert
+//         } catch { alert('Ошибка парсинга'); }
+//       };
+//     }
+//   }
 
-  onChangeTodosOnPage = count => () => {
-    const {
-      todos, isReverse,
-    } = this.state;
-    const pages = Math.ceil((todos.length + 1) / count);
-    this.setState({
-      todosOnPage: count,
-      pageNumber: isReverse ? 1 : pages,
-    });
-  }
+//   onChangePage = page => () => {
+//     this.setState({
+//       pageNumber: page,
+//     });
+//   }
 
-  render() {
-    const {
-      todos, isReverse, pageNumber, todosOnPage,
-    } = this.state;
-    const todosDir = isReverse ? todos.map(todo => todo).reverse() : todos.map(todo => todo);
-    const todosView = todosDir.slice((pageNumber - 1) * todosOnPage, pageNumber * todosOnPage);
-    const pageCount = todos.length === 0 ? 1 : Math.ceil(todos.length / todosOnPage);
-    return (
-      <Container fluid="falses">
-        <TodoInput
-          input={this.input}
-          onAppend={this.handlerAppendTodo}
-        />
-        <TodoList
-          todos={todosView.filter(todo => todo.isDone === false)}
-          onDone={this.handlerDoneTodo}
-          onRemove={this.handlerRemoveTodo}
-        />
-        <TodoList
-          todos={todosView.filter(todo => todo.isDone === true)}
-          onDone={this.handlerDoneTodo}
-          onRemove={this.handlerRemoveTodo}
-        />
-        <Paginator
-          currentPage={pageNumber}
-          pageCount={pageCount}
-          onChangePage={this.onChangePage}
-          todosOnPage={todosOnPage}
-          onChangeTodosOnPage={this.onChangeTodosOnPage}
-          isReverse={isReverse}
-          onReverse={this.handlerReverseSelect}
-        />
-        <HeaderMenu
-          onDownload={this.handlerFileDownload}
-          onUpload={this.handleFileUpload}
-        />
-      </Container>
-    );
-  }
-}
+//   onChangeTodosOnPage = count => () => {
+//     const {
+//       todos, isReverse,
+//     } = this.state;
+//     const pages = Math.ceil((todos.length + 1) / count);
+//     this.setState({
+//       todosOnPage: count,
+//       pageNumber: isReverse ? 1 : pages,
+//     });
+//   }
+
+//   render() {
+//     const {
+//       todos, isReverse, pageNumber, todosOnPage,
+//     } = this.state;
+//     const todosDir = isReverse ? todos.map(todo => todo).reverse() : todos.map(todo => todo);
+//     const todosView = todosDir.slice((pageNumber - 1) * todosOnPage, pageNumber * todosOnPage);
+//     const pageCount = todos.length === 0 ? 1 : Math.ceil(todos.length / todosOnPage);
+//     return (
+//       <Container fluid="falses">
+//         <TodoInput
+//           input={this.input}
+//           onAppend={this.handlerAppendTodo}
+//         />
+//         <TodoList
+//           todos={todosView.filter(todo => todo.isDone === false)}
+//           onDone={this.handlerDoneTodo}
+//           onRemove={this.handlerRemoveTodo}
+//         />
+//         <TodoList
+//           todos={todosView.filter(todo => todo.isDone === true)}
+//           onDone={this.handlerDoneTodo}
+//           onRemove={this.handlerRemoveTodo}
+//         />
+//         <Paginator
+//           currentPage={pageNumber}
+//           pageCount={pageCount}
+//           onChangePage={this.onChangePage}
+//           todosOnPage={todosOnPage}
+//           onChangeTodosOnPage={this.onChangeTodosOnPage}
+//           isReverse={isReverse}
+//           onReverse={this.handlerReverseSelect}
+//         />
+//         <HeaderMenu
+//           onDownload={this.handlerFileDownload}
+//           onUpload={this.handleFileUpload}
+//         />
+//       </Container>
+//     );
+//   }
+// }
 
 
 export default TodoEdit;
+
+TodoEdit.propTypes = {
+
+  todos: propTypes.arrayOf(propTypes.object).isRequired,
+  isReverse: propTypes.bool.isRequired,
+  pageNumber: propTypes.number.isRequired,
+  todosOnPage: propTypes.number.isRequired,
+  handlerAppendTodo: propTypes.func.isRequired,
+  handlerDoneTodo: propTypes.func.isRequired,
+  handlerRemoveTodo: propTypes.func.isRequired,
+  onChangePage: propTypes.func.isRequired,
+  onChangeTodosOnPage: propTypes.func.isRequired,
+  handlerReverseSelect: propTypes.func.isRequired,
+  handlerFileDownload: propTypes.func.isRequired,
+  handleFileUpload: propTypes.func.isRequired,
+};
--- src/index.js
+++ src/index.js
@@ -15,9 +15,17 @@ const store = createStore({}, window.__REDUX_DEVTOOLS_EXTENSION__
   // eslint-disable-next-line no-underscore-dangle
   && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+const todos = [{
+  id: 1550935913612, date: '23.02.2019, 20:31:53', text: 'сегодня сделал все: пипи на папу, кака пару раз, ну и вообще. Подкармливаем смесью, сон 3-4 часа.', isDone: false,
+}, {
+  id: 1550935977335, date: '23.02.2019, 20:32:57', text: 'выпил пару бокалов вина я. Хорошо.', isDone: false,
+}, {
+  id: 1550936486626, date: '23.02.2019, 20:41:26', text: 'сейчас неспокойно ребенку. Час назад Лена кормила грудью и он уснул, но вот час прошел... Подкорм, грудь, все идет в дело. Пукал.', isDone: false,
+}];
+
 ReactDOM.render(
   <Provider store={store}>
-    <TodoEdit name="Task manager" />
+    <TodoEdit name="Task manager" todos={todos} isReverse={false} pageNumber={1} todosOnPage={10} handlerAppendTodo={() => {}} handlerDoneTodo={() => {}} handlerRemoveTodo={() => {}} onChangePage={() => {}} onChangeTodosOnPage={() => {}} handlerReverseSelect={() => {}} handlerFileDownload={() => {}} handleFileUpload={() => {}} />
   </Provider>,
   document.getElementById('root'),
 );
