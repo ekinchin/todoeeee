d2e9e86f794c048d048d2fa1e77eb2a06e612323
ekinchin <evgen4in@gmail.com>
2020-04-02 11:25:28 +0500

    добавлен в гит FileBlock

---
+++ src/components/FileBlock.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import propTypes from 'prop-types';
+import {
+  Row, Button, Container, ButtonGroup,
+} from 'react-bootstrap';
+
+const FileBlock = ({
+  onDownload, onUpload,
+}) => (
+  <Container fluid="true">
+    <Row className="justify-content-center align-items-center my-3" noGutters="true">
+      <ButtonGroup>
+        <input type="file" onChange={onUpload} />
+        <Button size="sm" variant="success" onClick={onDownload}>скачать</Button>
+      </ButtonGroup>
+    </Row>
+  </Container>
+);
+
+export default FileBlock;
+
+FileBlock.propTypes = {
+  onDownload: propTypes.func.isRequired,
+  onUpload: propTypes.func.isRequired,
+};
