60f99a3508dd657ca5b1d9d3b91594d035fa9f8e
ekinchin <evgen4in@gmail.com>
2020-04-26 12:12:37 +0500

    добавлен date-fns для отображения времени записи

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { format } from 'date-fns';
+
 import {
   Row, Col, Button, Card,
 } from 'react-bootstrap';
 
 const TodoItem = ({ todo, onRemove }) => (
   <Row className="justify-content-center align-items-center text-left my-3" noGutters="true">
-    <Col className="col-auto mx-1">{todo.date}</Col>
+    <Col className="col-auto mx-1">
+      {format(todo.id, 'MM.dd.yyyy HH:MM:SS')}
+    </Col>
     <Col className="col-6">
       <Card bg="light" text="dark">
         <Card.Body className="p-1">
