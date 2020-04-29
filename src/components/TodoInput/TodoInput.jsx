c11a82ab778aea6154244ce4046e2b2ee768e2f3
ekinchin <evgen4in@gmail.com>
2020-04-29 22:54:58 +0500

    избавление от bootstrap базовая стилизация

---
+++ src/components/TodoInput/TodoInput.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/no-autofocus */
+import React from 'react';
+import propTypes from 'prop-types';
+import './style.css';
+
+const TodoInput = ({ onAppend }) => {
+  const input = React.createRef();
+  return (
+    <form id="userinput" className="formInput">
+      <input type="text" className="formInput--input" ref={input} placeholder="ТуДу ждет" autoFocus />
+      <button type="submit" className="formInput--submit" form="userinput" onClick={(e) => { e.preventDefault(); onAppend(input); }}>
+          Добавить
+      </button>
+    </form>
+  );
+};
+
+export default TodoInput;
+
+TodoInput.propTypes = {
+  onAppend: propTypes.func.isRequired,
+};
