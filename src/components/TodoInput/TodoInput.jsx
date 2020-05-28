201d2722fa18a50df963dad1bce64f62df412777
ekinchin <evgen4in@gmail.com>
2020-05-28 22:59:34 +0500

    alingment flexbox

--- src/components/TodoInput/TodoInput.jsx
+++ src/components/TodoInput/TodoInput.jsx
@@ -6,15 +6,17 @@ import './style.css';
 const TodoInput = ({ onAppend }) => {
   const input = React.createRef();
   return (
-    <form id="userinput" className="formInput">
-      <input type="text" className="formInput--input" ref={input} placeholder="ТуДу ждет" autoFocus />
-      <button type="submit" className="formInput--submit" form="userinput" onClick={(e) => { e.preventDefault(); onAppend(input); }}>
-        <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
-          <path d="M 16,5 V 26" stroke="black" strokeWidth="2" />
-          <path d="M 5,16 H 26" stroke="black" strokeWidth="2" />
-        </svg>
-      </button>
-    </form>
+    <div className="formInput--container">
+      <form id="userinput" className="formInput">
+        <input type="text" className="formInput--input" ref={input} placeholder="ТуДу ждет" autoFocus />
+        <button type="submit" className="formInput--submit" form="userinput" onClick={(e) => { e.preventDefault(); onAppend(input); }}>
+          <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
+            <path d="M 16,5 V 26" stroke="black" strokeWidth="2" />
+            <path d="M 5,16 H 26" stroke="black" strokeWidth="2" />
+          </svg>
+        </button>
+      </form>
+    </div>
   );
 };
 
