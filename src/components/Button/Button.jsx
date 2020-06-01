a7530227a9f37f21a1112b11240c234fbb2759bd
ekinchin <evgen4in@gmail.com>
2020-06-01 23:17:08 +0500

    added new component Button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-no-bind */
+import React from 'react';
+import propTypes from 'prop-types';
+
+import './style.css';
+
+const Button = ({ onClick, label = '' }) => (
+  <button className="fileBlock--button" type="button" onClick={() => onClick()}>{label}</button>
+);
+
+export default Button;
+
+Button.propTypes = {
+  onClick: propTypes.func.isRequired,
+  label: propTypes.isRequired,
+};
