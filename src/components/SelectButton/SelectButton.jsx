3f005724724fc823c399561c63c9a8d16fd51a92
ekinchin <evgen4in@gmail.com>
2020-06-14 23:58:39 +0500

    добален способ задать активный пункт списка

--- src/components/SelectButton/SelectButton.jsx
+++ src/components/SelectButton/SelectButton.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './style.css';
 
-const SelectButton = ({ pairs, onSelect }) => (
-  <select
-    className="selectButton"
-    onChange={(e) => {
-      onSelect(Number.parseInt(e.target.value, 10));
-    }}
-  >
-    {pairs.map(pair => <option className="selectButton--option" key={pair.label} value={pair.value}>{pair.label}</option>)}
-  </select>
-);
+const SelectButton = ({ pairs, onSelect }) => {
+  const activeValue = pairs.reduce((value, pair) => (pair.active ? value + pair.value : value), 0);
+  return (
+    <select
+      className="selectButton"
+      onChange={(e) => {
+        onSelect(Number.parseInt(e.target.value, 10));
+      }}
+      value={activeValue}
+    >
+      {pairs.map(pair => <option className="selectButton--option" key={pair.label} value={pair.value}>{pair.label}</option>)}
+    </select>
+  );
+};
 
 export default SelectButton;
 
 SelectButton.defaultProps = {
-  pairs: [{ label: 1, value: 11 }, { label: 2, value: 12 }, { label: 3, value: 13 }],
+  pairs: [
+    { label: 1, value: 11, active: true },
+    { label: 2, value: 12, active: false },
+    { label: 3, value: 13, active: false },
+  ],
   onSelect: () => { },
 };
 
