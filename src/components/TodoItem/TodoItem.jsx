aa9011f73cc42e8891ab34b49196777f6ffd45ea
ekinchin <evgen4in@gmail.com>
2020-06-15 22:24:36 +0500

    локализация отображения даты

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { format } from 'date-fns';
+import { ru, enGB } from 'date-fns/locale';
 import './style.css';
+
+const locale = { locale: navigator.language === 'ru-RU' ? ru : enGB }; // получить локаль браузера
 
 const TodoItem = ({ todo, onRemove }) => (
   <div className="item">
     <span className="item--date">
-      {format(todo.id, 'dd.MM.yyyy HH:MM:SS')}
+      {format(todo.id, 'dd MMMM yyyy HH:MM:SS', locale).toLocaleLowerCase()}
     </span>
     <div className="item--text">
       <span>{todo.text}</span>
